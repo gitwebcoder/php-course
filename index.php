1797e4e965308e9e485260dd2a5083f8c3fc0b34
gitwebcoder <54285749+gitwebcoder@users.noreply.github.com>
2021-06-11 10:42:20 -0400

    adding HTML and PHP code

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <title> PHP Store</title>
+  </head>
+  <body>
+    <?php
+      //Using comments
+      echo "<h1>Welcome!</h1>";
+
+     ?>
+  </body>
+
+</html>
